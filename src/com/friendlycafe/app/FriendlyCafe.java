06167517575bdc698c078e93434b1696e1554cde
Prasanth S <prasanthshanmugam1402@gmail.com>
2025-03-05 00:00:04 +0000

    GUI Basic Formation

--- src/com/friendlycafe/app/FriendlyCafe.java
+++ src/com/friendlycafe/app/FriendlyCafe.java
@@ -4,17 +4,23 @@
  */
 package com.friendlycafe.app;
 
-
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.util.ArrayList;
-import java.util.HashMap;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.friendlycafe.pojo.Customer;
 import com.friendlycafe.pojo.Item;
 import com.friendlycafe.service.DataService;
-import com.friendlycafe.exceptions.*;
 
 /**
  * 
@@ -33,29 +39,59 @@ public class FriendlyCafe {
 		for(Item item : menu) {
 			appLogger.info("Item Name : "+item.name);
 		}
-
 		
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel welcomeLabel = new JLabel("Welcome to The Friendly Cafe");
+		welcomeLabel.setPreferredSize(new Dimension(200,20));
+		welcomeLabel.setBounds(1, 20, 80, 25);
+		welcomeLabel.setLayout( new GridLayout());
+		JLabel mailLabel = new JLabel("Mail ID : ");
+		mailLabel.setBounds(10, 20, 80, 25);
+		JFormattedTextField mailId = new JFormattedTextField();
+		mailId.setPreferredSize(new Dimension(200,20));
+		mailId.setBounds(100, 20, 180, 25);
+		mailId.setLayout( new GridLayout(30, 30) );
+//		panel.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 2, true));
+		panel.setAutoscrolls(true);
+		panel.setPreferredSize(new Dimension(100,100));
+		panel.add(welcomeLabel);
+		panel.add(mailLabel);
+		panel.add(mailId);
+		String[] menus = new String[menu.size()];
+		for(int index = 0; index < menu.size(); index++) {
+			menus[index] = menu.get(index).toString(); 
+		}
+		JList<String> view = new JList<>(menus);
+		JScrollPane pane = new JScrollPane(view);
+		panel.add(pane);
+		String[] columns = new String[] {"Item Name", "Description", "Price"};
+		
+		DefaultTableModel tableModel = new DefaultTableModel();
+		
+		frame.setTitle("The Friendly cafe");
+		frame.add(panel);
+		frame.setBounds(50, 50, 50, 50);
+		frame.setPreferredSize(new Dimension(500,500));
+		frame.pack();
+		frame.setVisible(true);
+
+/*			
 		// GET THE BELOW TWO VALUES FROM GUI
+		
 		HashMap<String, Integer> orderingItem = new HashMap<>();
+	
 		orderingItem.put(menu.get(0).itemId, 4);
 		orderingItem.put(menu.get(2).itemId, 4);
 		
-		Customer customer = new Customer("aaa", "dsa@gmail.com");
-		// GET THE ABOVE customer and orderingItems TWO VALUES FROM GUI
 		
-		try{
-			if(!service.checkCustomer(customer.mailId))
-				throw new CustomerFoundException("Customer with Mail ID: " + customer.mailId + " already exists");
-			else {
-				service.saveCustomerDetails("aaa", "aaa@aaa.com");
-				appLogger.info("Customer Details Saved");
-			}
-		}
-		catch (CustomerFoundException e){
-			appLogger.error("Exception came : "+e.getMessage());
-			}
+		Customer customer = new Customer("aaa", "dsa@gmail.com");
+		
+		if(!service.checkCustomer(customer.mailId))
+			service.saveCustomerDetails("aaa", "aaa@aaa.com");
+		
 		service.saveOrder(customer.mailId, orderingItem);
-	
+*/	
 		
 //		service.generateReport();
 	}
